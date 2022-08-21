342ad00b65cd16136a74cab1f0dd8f525e314235
unknown <biplob2358@gmail.com>
2022-08-22 02:17:05 +0600

    app.js file added

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const cartArray = [];
+
+//display function
+function diplayPlayer(cartPlayers) {
+  const tableBody = document.getElementById("cart-player");
+  tableBody.innerHTML = "";
+  for (let i = 0; i < cartPlayers.length; i++) {
+    const name = cartArray[i].palyerName;
+
+    const tr = document.createElement("tr");
+    if (cartPlayers.length > 5) {
+      alert("No more selection left");
+      location.reload();
+      return;
+    }
+    tr.innerHTML = `
+    <th>${i + 1}</th>
+    <th>${name}</th>
+    `;
+    tableBody.appendChild(tr);
+  }
+}
+//onclick function
+function selectBtn(element) {
+  const palyerName = element.parentNode.parentNode.children[0].innerText;
+  element.disabled = true;
+
+  const playerObj = {
+    palyerName: palyerName,
+  };
+  cartArray.push(playerObj);
+  diplayPlayer(cartArray);
+}
